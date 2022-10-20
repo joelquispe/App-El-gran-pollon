ef5c829672315a1eba6e028fa8e516f8e5a9fd0b
Saiyan2003 <samuelcraft2003@gmail.com>
2022-10-20 01:37:39 -0500

    Changes Design Login and Register

--- app/src/main/java/com/example/appelgranpollon/network/RestEngine.kt
+++ app/src/main/java/com/example/appelgranpollon/network/RestEngine.kt
@@ -24,7 +24,7 @@ class RestEngine {
             cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
             val cliente = OkHttpClient.Builder().cookieJar(JavaNetCookieJar(cookieManager)).followRedirects(false).addInterceptor(interceptor).build();
 
-            val retrofit = Retrofit.Builder().baseUrl("http://192.168.18.45:8090/").addConverterFactory(
+            val retrofit = Retrofit.Builder().baseUrl("http://192.168.1.3:8090/").addConverterFactory(
                 GsonConverterFactory.create()).client(cliente).build();
             return retrofit;
         }
